d9edcfe3317dad8ff65ee0af9926f35251ac66ce
Taynan Z.Hott <45300380+taynanhott@users.noreply.github.com>
2024-06-19 23:44:31 -0300

    sqlite integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import openDB from "../configdb";
 
 const inter = Inter({ subsets: ["latin"] });
-
-openDB();
 
 export const metadata: Metadata = {
   title: "Project Base",
